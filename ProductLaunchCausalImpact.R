c9a05554078123de01f2903a0a04410dac114ee7
christosvis <110899985+christosvis@users.noreply.github.com>
2022-08-21 19:27:15 +0300

    Update ProductLaunchCausalImpact.R

--- ProductLaunchCausalImpact.R
+++ ProductLaunchCausalImpact.R
@@ -1,25 +1,13 @@
-# https://google.github.io/CausalImpact/CausalImpact.html
-
 #load library
 library(CausalImpact)
 library(pivottabler)
-
-# Example dataset
-# set.seed(1)
-# x1 <- 100 + arima.sim(model = list(ar = 0.999), n = 100)
-# y <- 1.2 * x1 + rnorm(100)
-# y[71:100] <- y[71:100] + 10
-# data <- cbind(y, x1)
-
-# import data
-# data <-  read.csv('/Users/christos/Downloads/oura-launch.csv')
 
 # import data and choose file
 df <- read.csv(file.choose())
 
 # pivot table
 # arguments:  qpvt(dataFrame, rows, columns, calculations, ...)
-pivot1 <- qpvt(df, "NPU_DATE", "COUNTRY", "sum(SUM.PLATFORM_NPU.)") 
+pivot1 <- qpvt(df, "AppInstalls", "COUNTRY", "sum(SUM.PLATFORM_NPU.)") 
 pivot1
 
 # convert to data frame 
@@ -42,8 +30,6 @@ df1
 us <- ts(df1$US)
 gb <- ts(df1$GB)
 au <- ts(df1$AU)
-se <- ts(df1$SE)
-row <- ts(df1$ROW)
 
 # bind data
 # data <- cbind(us, gb) 
@@ -53,10 +39,6 @@ row <- ts(df1$ROW)
 
 # visualize
 # matplot(data, type = "l")
-
-# Set pre and post periods (no dates)
-# pre.period <- c(1, 32)
-# post.period <- c(33, 35)
 
 # dates
 # nrow() numberof rows
@@ -71,7 +53,7 @@ data <- zoo(cbind(us, gb), time.points)
 # head(data)
 
 #############
-# create data for correlation until August 1
+# create data for correlation until date you want. Change the number of last rows
 data.cor <- head(data, - 6)          
 data.cor
 
@@ -84,8 +66,9 @@ cor(data.cor)
 # visualize
 matplot(data, type = "l")
 
-pre.period <- as.Date(c("2022-07-01", "2022-08-02"))
-post.period <- as.Date(c("2022-08-03", "2022-08-07"))
+# set pre and post periods. Change dates accordingly
+pre.period <- as.Date(c("2022-01-01", "2022-02-02"))
+post.period <- as.Date(c("2022-02-03", "2022-02-07"))
 
 # run causal inference analysis
 impact <- CausalImpact(data, pre.period, post.period)
